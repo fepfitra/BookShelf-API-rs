e13b5e19ba75388ed99b405ba51caa04efb76235
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-10-26 01:21:01 +0700

    log: add on response with path, response code, latency, and method

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use axum::{
     Router,
+    extract::{MatchedPath, Request},
     routing::{get, post},
 };
 use std::sync::Arc;
+use tower_http::trace::TraceLayer;
+use tracing::info_span;
 use tracing_subscriber::prelude::*;
 
 mod utils;
@@ -19,8 +22,9 @@ use services::book::{
 async fn main() {
     tracing_subscriber::registry()
         .with(
-            tracing_subscriber::EnvFilter::try_from_default_env()
-                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
         )
         .with(tracing_subscriber::fmt::layer())
         .init();
@@ -35,7 +39,24 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(root))
-        .nest("/books", book_router);
+        .nest("/books", book_router)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(()),
+        );
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await

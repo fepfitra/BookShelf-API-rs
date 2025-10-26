54a570b4285e5086ab4ee5d4cd76304c4a0aec3c
fepfitra <fitriansyahekaputra2001@mail.ugm.ac.id>
2025-10-26 10:22:03 +0700

    ref: split app into different file

---
+++ src/app.rs
@@ -0,0 +1,50 @@
+use std::sync::Arc;
+
+use axum::{
+    Router,
+    extract::{MatchedPath, Request},
+    routing::{get, post},
+};
+use tower_http::trace::TraceLayer;
+use tracing::info_span;
+
+use crate::services::book::{
+    handler::{create_book, delete_book, get_book_by_id, update_book},
+    repo::InMemoryBookRepo,
+};
+use crate::{root, services::book::BookState};
+
+pub fn app() -> Router {
+    let book_repo = InMemoryBookRepo::default();
+    let book_router = Router::new()
+        .route("/", post(create_book).get(get_book_by_id))
+        .route(
+            "/{id}",
+            get(get_book_by_id).put(update_book).delete(delete_book),
+        )
+        .with_state(BookState {
+            repo: Arc::new(book_repo),
+        });
+
+    let app = Router::new()
+        .route("/", get(root))
+        .nest("/books", book_router)
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(|request: &Request<_>| {
+                    let path = request
+                        .extensions()
+                        .get::<MatchedPath>()
+                        .map(MatchedPath::as_str);
+
+                    info_span!(
+                        "http_request",
+                        method = ?request.method(),
+                        path,
+                        some_other_field = tracing::field::Empty,
+                    )
+                })
+                .on_request(()),
+        );
+    app
+}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use axum::{
-    Router,
-    extract::{MatchedPath, Request},
-    routing::{get, post},
-};
-use std::sync::Arc;
-use tower_http::trace::TraceLayer;
-use tracing::info_span;
-use tracing_subscriber::prelude::*;
-
-mod utils;
-pub use utils::error::AppError;
-
+mod app;
 mod services;
-use services::book::{
-    BookState,
-    handler::{create_book, delete_book, get_book_by_id, get_books, update_book},
-    repo::InMemoryBookRepo,
-};
+mod utils;
+
+use app::app;
+use tracing_subscriber::prelude::*;
+pub use utils::error::AppError;
 
 #[tokio::main]
 async fn main() {
@@ -29,43 +17,11 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let book_repo = InMemoryBookRepo::default();
-    let book_router = Router::new()
-        .route("/", post(create_book).get(get_books))
-        .route(
-            "/{id}",
-            get(get_book_by_id).put(update_book).delete(delete_book),
-        )
-        .with_state(BookState {
-            repo: Arc::new(book_repo),
-        });
-
-    let app = Router::new()
-        .route("/", get(root))
-        .nest("/books", book_router)
-        .layer(
-            TraceLayer::new_for_http()
-                .make_span_with(|request: &Request<_>| {
-                    let path = request
-                        .extensions()
-                        .get::<MatchedPath>()
-                        .map(MatchedPath::as_str);
-
-                    info_span!(
-                        "http_request",
-                        method = ?request.method(),
-                        path,
-                        some_other_field = tracing::field::Empty,
-                    )
-                })
-                .on_request(()),
-        );
-
     let listener = tokio::net::TcpListener::bind("127.0.0.1:5000")
         .await
         .unwrap();
     tracing::info!("Server running on http:{}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app()).await.unwrap();
 }
 
 async fn root() -> &'static str {
